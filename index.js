b2471accf34dcc28dabea1a6c307142f603d5723
Ilia Tsymbal <itsymbal@exadel.com>
2017-08-04 21:42:40 +0300

    Serve files

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const http = require('http');
+const http = require('http'),
+  config = require('./config/default.json'),
+  fileServer = require('./fileServer'),
+  paths = config.paths,
+  names = config.fileNames;
 
-const server = http.createServer((req, res) => {
-  res.end('Hello');
+const server = http.createServer((request, response) => {
+  let file = [__dirname, paths.views, request.url].join('/');
+
+  if (request.url === '/') {
+    file = [__dirname, paths.views, names.mainHTML].join('/');
+  }
+
+  fileServer(file, request, response);
 });
 
 server.listen(process.env.PORT || 5000);
